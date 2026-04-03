1fd17ae8a1763ad99f2ed77626d9fa34b6a57ea0
genjihakura <phattruongspkt@gmail.com>
2026-04-03 17:03:00 +0700

    modify test case cmd_line_parser()

--- project_unit_test/test/test_cmd_line.c
+++ project_unit_test/test/test_cmd_line.c
@@ -20,49 +20,47 @@ cmd_line_t cmd_table[] = {
     {(uint8_t*)"help", mock_func,(const char*) "HELP"},
     {(uint8_t*)"led", mock_func,(const char*) "LED"},
     {(uint8_t*)"uart", mock_func, (const char*) "UART"},
-    {0, 0}
+    {(uint8_t*)"null", (cmd_line_t*)0, (const char*) "UART"},
+    {0,0,0}
 };
 
 typedef struct {
     char * name_test;
-	pf_cmd_func func;
-    uint8_t* cmd;
+    cmd_line_t *data;
+    uint8_t* command;
     int8_t expected_result;
 }test_case_t;
 
 test_case_t test_case[] = {
-    {"Call Function CMD_SUCCESS", mock_func,(uint8_t*)"help", CMD_SUCCESS},
-    {"Call Function CMD_TBL_NOT_FOUND", (cmd_line_t*)0, (uint8_t*)"help",CMD_TBL_NOT_FOUND},
-    {"Call Function CMD_TOO_LONG", mock_func, (uint8_t*)"help help help help",CMD_TOO_LONG},
-    {"Call Function CMD_NOT_FOUND", mock_func, (uint8_t*)"led On",CMD_NOT_FOUND}
+    {"Call Function CMD_SUCCESS",cmd_table, (uint8_t*)"help" , CMD_SUCCESS},
+    {"Call Function CMD_TBL_NOT_FOUND",(cmd_line_t*)0,"help",CMD_TBL_NOT_FOUND},
+    {"Call Function CMD_TOO_LONG",cmd_table,(uint8_t*)"aaaaaaaaaaaaaaaaaaaaaaaaaaaaa",CMD_TOO_LONG},
+    {"Call Function CMD_NOT_FOUND",cmd_table,(uint8_t*)"uart2",CMD_NOT_FOUND}
 };
 
-void run_test(test_case_t *tc){
-    cmd_line_t * test_data = malloc(size(cmd_line_parser));
-    test_data->func = tc->func;
-    test_data->cmd = tc->cmd;
+void run_test(test_case_t tc){
+    func_called =0;
+   //printf("\s", tc.command);
+    uint8_t ret = cmd_line_parser(tc.data,tc.command);
 
-    uint8_t ret = cmd_line_parser(test_data,tc->cmd);
-
-    if(ret != tc->expected_result){
-        printf("[FAIL]: %s: expected: %d actual: %d", tc->name_test, tc->expected_result, ret);
+    if(ret != tc.expected_result){
+        printf("[FAIL]: %s: expected: %d actual: %d\n", tc.name_test, tc.expected_result, ret);
     }
 
-    if(ret == tc->expected_result){
-        printf("[PASS]: %s  expected: %d actual: %d", tc->name_test, tc->expected_result, ret);
+    if(ret == tc.expected_result){
+        printf("[PASS]: %s  expected: %d actual: %d\n", tc.name_test, tc.expected_result, ret);
     }
-
-    // return;
+    printf("\n");
 }
 
 
 int main(){
     int total_testcase = sizeof(test_case)/ sizeof(test_case[0]);
     printf("Runing test all test case: %d\n", total_testcase);
-    for(int i = 0 ; i < total_testcase; i++){
+    for(int i = 1 ; i <= total_testcase; i++){
         printf("Runing test %d test case:\n", i);
-        run_test(&test_case[i]);
+        run_test(test_case[i-1]);
     }
-
+    printf("Done\n");
     return 0;
 }
